4b0efdb05d202ec6a20fa60284266765f8f4614c
erinnlebaron <erinnlebaron3@gmail.com>
2019-12-19 14:37:36 -0700

    react project

--- src/components/app.js
+++ src/components/app.js
@@ -71,6 +71,10 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [<Route path="/blog" component={Blog} />];
+  }
+
   render() {
     return (
       <div className="container">
@@ -96,7 +100,9 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+                this.authorizedPages() 
+              ) : null}
               <Route
                 exact
                 path="/portfolio/:slug"
