f38be35009064b84767cef5270b0cd51470cac19
erinnlebaron <erinnlebaron3@gmail.com>
2020-01-10 11:49:54 -0700

    contact pg react proj

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,13 +1,26 @@
-import { 
-    faTrash, 
-    faSignOutAlt, 
-    faEdit, faSpinner, 
-    faPlusSquare
-} from "@fortawesome/free-solid-svg-icons"; 
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-const Icons = () => {
- return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusSquare);
-};
-
-export default Icons;
+import {
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+  } from "@fortawesome/free-solid-svg-icons";
+  import { library } from "@fortawesome/fontawesome-svg-core";
+  
+  const Icons = () => {
+    return library.add(
+      faTrash,
+      faSignOutAlt,
+      faEdit,
+      faSpinner,
+      faPlusCircle,
+      faPhone,
+      faEnvelope,
+      faMapMarkedAlt
+    );
+  };
+  
+  export default Icons;
